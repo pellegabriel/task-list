1b1c9956c2073df7217b84fc2e885694b34996bf
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-11-09 17:28:05 -0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [newTaskName, setNewTaskName] = useState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder='New Task...'
+        onChange={(e) => setNewTaskName(e.target.value)}
+      />
+      <button onClick={() => alert (newTaskName)}>Save Task</button>
     </div>
   );
 }
