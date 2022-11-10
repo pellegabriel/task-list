33f5ad3adfd304a2a7b874aefe6cca600709ab9c
pellegabriel <gabypelle12@gmail.com>
2022-11-10 02:04:47 +0000

    Update App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-import { useState } from 'react';
-import './App.css';
-import TaskCreator from './components/taskCreator';
+import { useState } from "react";
+import "./App.css";
+import TaskCreator from "./components/taskCreator";
 
 function App() {
-
-  const [tasksItems, setTasksItems] = useState ([
-    {name: 'MyTask 1', done: false},
-    {name: 'MyTask 2', done: false},
-    {name: 'MyTask 3', done: false},
-  ])
+  const [tasksItems, setTasksItems] = useState([
+    { name: "MyTask 1", done: false },
+    { name: "MyTask 2", done: false },
+    { name: "MyTask 3", done: false },
+  ]);
 
   function createNewTask(taskName) {
-    if (!tasksItems.find(task => task.name === taskName)){
-      setTasksItems([...tasksItems, {name:taskName, done:false}])
+    if (!tasksItems.find((task) => task.name === taskName)) {
+      setTasksItems([...tasksItems, { name: taskName, done: false }]);
     }
   }
 
   return (
     <div className="App">
-      <TaskCreator
-        createNewTask={createNewTask} 
-      />
-      <table>
+      <TaskCreator createNewTask={createNewTask} />
+      <table className="table">
         <thead>
           <h3>Tasks</h3>
         </thead>
         <tbody>
-        {
-          tasksItems.map(task =>(
+          {tasksItems.map((task) => (
             <tr key={task.name}>
-              <td>
-              {task.name}
-              </td>
+              <td>{task.name}</td>
             </tr>
-          ))
-        }
+          ))}
         </tbody>
       </table>
     </div>
